a87640e80018c5a7dc30484e87eaa1d4cf8584b0
Umakambampati <kambampatiumamaheshwari@gmail.com>
2025-11-16 21:25:19 +0530

    Restore old files from previous commit

---
+++ control.py
@@ -0,0 +1,126 @@
+# num=int(input("Enter any number"))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+# num1=int(input("Enter a number"))
+# num2=int(input("Enter a number"))
+# if num1==num2:
+#     print("Both are equal")
+# elif num1>num2:
+#     print("The greatest number is",num1)
+# else:
+#     print("The greatest number is",num2)
+# num1=int(input("Enter a number"))
+# num2=int(input("Enter a number"))
+# num3=int(input("Enter a number"))
+# if num1==num2==num3:
+#     print("All are equal")
+# elif num1>num2 and num1>num3:
+#     print("The largest number is",num1)
+# elif num2>num1 and num2>num3:
+#     print("The largest number is",num2)
+# else:
+#     print("The largest number is",num3)
+# number=int(input("Enter a number"))
+# if number>0:
+#     print("Positive")
+# else:
+#     if number==0:
+#         print("Zero")
+#     elif number<0:
+#         print("Negative")
+# num=int(input("enter any number"))
+# sum=0
+# for i in range(num+1):
+#     sum+=1
+# print(sum)
+# number=int(input("enter any number"))
+# for i in range(1,number+1):
+#     if i%2==0:
+#         print(i,end=" ")
+# num1=int(input("enter any number"))
+# num=1
+# while num<=10:
+#     print(f"{num1}x{num}={num1*num}")
+#     num+=1
+# count=1
+# while count<=10:
+#     print(count,end=" ")
+#     count=count+1
+# num=int(input("Enter any number"))
+# sum=0
+# i=1
+# while i<=num:
+#     sum=sum+i
+#     i=i+1
+# print(sum)
+# password=input("enter  password")
+# actual_password="uma kambampati"
+# password=input("enter  password")
+# actual_password="uma kambampati"
+# for i in range(3):
+#     password=input("enter password")
+#     if password==actual_password:
+#         print("login succesful")
+#         break
+#     else:
+#         print("incorrect password")
+# else:
+#     print("Account locked")
+
+# for i in range(1,21):
+#     if i%3==0:
+#         continue
+#     print(i,end=
+# list1=list(input("enter list"))
+# count1=0
+# count2=0
+# for i in list1:
+#     if i%2==0:
+#         count1=count1+i
+#     else:
+#         count2+i
+# print(count1)
+# print(count2
+# str1=input("enter any word")
+# for char in str1:
+#     print(char)
+# num=int(input("enter any number"))
+# if num%2==0 and num%5==0:
+#     print(num, "is divisible by both")
+# else:
+#     print("not divisible")
+# class_no=1
+# while  class_no<11:
+#     roll_no=1
+#     while roll_no<31:
+#         print('class',class_no,'roll',roll_no)
+#         roll_no+=1
+#     class_no+=1
+# for i in range(1,20):
+#     print(i*2567)
+    
+#     break
+# def music_medicine():
+#     print("Music makes mind calm and relaxed")
+#     print("Listeing to devotional songs feels like heaven")
+# music_medicine()
+# print("Dance isalsoa form of art")
+# music_medicine()
+# def simple_calculator(a,b):
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+#     print(a//b)
+#     print(a%b)
+#     print(a^b)
+# simple_calculator(3,6)
+# simple_calculator(4,7)
+# simple_calculator(9,2)
+# simple_calculator(1,5)
+# def sum_of_numbers(num):
+#     sum=(num*(num+1))/2
+#     print(sum)
+# sum_of_numbers(5)
+# sum_of_numbers(6)
